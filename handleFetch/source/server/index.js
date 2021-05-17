929c56a62242e7f590544d4d5ec6731b0d007322
w3liangxn <gfr6248@dingtalk.com>
2021-05-17 11:09:26 +0800

    更新下

--- handleFetch/source/server/index.js
+++ handleFetch/source/server/index.js
@@ -7,35 +7,34 @@ let server = http.createServer()
 server.on('request', function (req, res) {
   const url = req.url
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Headers', '*')
 
-  if (url === '/') {
-    fs.readFile(path.resolve(__dirname, '../index.html'), "utf-8", function (err, data) {
-      if (err) {
-        res.statusCode = 404;
-        res.setHeader("Content-Type","text/plain")
-        res.end("Not Found!")
-        return
-      }
-      res.setHeader("Content-Type", "text/html")
-      res.statusCode = 200
-      res.end(data)
-    })
-  }
+  // if (url === '/') {
+  //   fs.readFile(path.resolve(__dirname, '../index.html'), "utf-8", function (err, data) {
+  //     if (err) {
+  //       res.statusCode = 404;
+  //       res.setHeader("Content-Type","text/plain")
+  //       res.end("Not Found!")
+  //       return
+  //     }
+  //     res.setHeader("Content-Type", "text/html")
+  //     res.statusCode = 200
+  //     res.end(data)
+  //   })
+  // }
 
-  if (url === '/limitPromise.js') {
-    fs.readFile(path.resolve(__dirname, '../limitPromise.js'), "utf-8", function (err, data) {
-      if (err) {
-        res.statusCode = 404;
-        res.setHeader("Content-Type","text/plain")
-        res.end("Not Found!")
-        return
-      }
-      res.setHeader("Content-Type", "text/javascript")
-      res.statusCode = 200
-      res.end(data)
-    })
-  }
+  // if (url === '/limitPromise.js') {
+  //   fs.readFile(path.resolve(__dirname, '../limitPromise.js'), "utf-8", function (err, data) {
+  //     if (err) {
+  //       res.statusCode = 404;
+  //       res.setHeader("Content-Type","text/plain")
+  //       res.end("Not Found!")
+  //       return
+  //     }
+  //     res.setHeader("Content-Type", "text/javascript")
+  //     res.statusCode = 200
+  //     res.end(data)
+  //   })
+  // }
 
   if (url.includes('/api/mock')) {
     const number = url.split('=')[1]
